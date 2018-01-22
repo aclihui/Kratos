866c6eeab5408e7c360d1103049afe7ee0f9c1d2
小白-白 <xb@fczbl.vip>
2018-01-22 14:02:00 +0800

    主要对手机端界面进行了大整改

--- header-banner.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * The template for displaying the header
- *
- * @package Vtrois
- * @version 2.5(17.12.29)
- */
-			echo '<div class="kratos-start kratos-hero-2"><div class="kratos-overlay"></div><div class="kratos-cover kratos-cover_2 text-center" style="background-image: url(' . kratos_option('background_image') . ');"><div class="desc desc2 animate-box"><a href="'.get_bloginfo('url').'"><h2>' . kratos_option('background_image_text1') . '</h2><br><span>' . kratos_option('background_image_text2') . '</span></a></div></div></div>';
-?>
