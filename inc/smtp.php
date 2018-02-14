f5450346d681200ce98be86b642e3d683dda57cb
小白-白 <xb@fczbl.vip>
2018-02-14 13:41:03 +0800

    评论回复通知邮件中的表情符号 :xxx: 将会转义为表情图片

--- inc/smtp.php
+++ inc/smtp.php
@@ -1,40 +1,41 @@
 <?php
 
-add_action('phpmailer_init', 'mail_smtp');
-function mail_smtp( $phpmailer ) {
-    if(kratos_option('mail_smtps') == 1){
+add_action('phpmailer_init','mail_smtp');
+function mail_smtp($phpmailer){
+    if(kratos_option('mail_smtps')==1){
         $mail_name = kratos_option('mail_name');
         $mail_host = kratos_option('mail_host');
         $mail_port = kratos_option('mail_port');
         $mail_username = kratos_option('mail_username');
         $mail_passwd = kratos_option('mail_passwd');
         $mail_smtpsecure = kratos_option('mail_smtpsecure');
-        $phpmailer->FromName = $mail_name ? $mail_name : 'Kratos'; 
-        $phpmailer->Host = $mail_host ? $mail_host : 'smtp.vtrois.com';
-        $phpmailer->Port = $mail_port ? $mail_port : '994';
-        $phpmailer->Username = $mail_username ? $mail_username : 'no_reply@vtrois.com';
-        $phpmailer->Password = $mail_passwd ? $mail_passwd : '123456789';
-        $phpmailer->From = $mail_username ? $mail_username : 'no_reply@vtrois.com';
-        $phpmailer->SMTPAuth = kratos_option('mail_smtpauth')==1 ? true : false ;
-        $phpmailer->SMTPSecure = $mail_smtpsecure ? $mail_smtpsecure : 'ssl';
+        $phpmailer->FromName = $mail_name?$mail_name:'犬の窝-邮件通知'; 
+        $phpmailer->Host = $mail_host?$mail_host:'smtp.office365.com';
+        $phpmailer->Port = $mail_port?$mail_port:'587';
+        $phpmailer->Username = $mail_username?$mail_username:'no_reply@fczbl.vip';
+        $phpmailer->Password = $mail_passwd?$mail_passwd:'123456789';
+        $phpmailer->From = $mail_username?$mail_username:'no_reply@fczbl.vip';
+        $phpmailer->SMTPAuth = kratos_option('mail_smtpauth')==1?true:false ;
+        $phpmailer->SMTPSecure = $mail_smtpsecure?$mail_smtpsecure:'ssl';
         $phpmailer->IsSMTP();
     }
 }
-add_action('comment_unapproved_to_approved', 'kratos_comment_approved');
-function kratos_comment_approved($comment) {
-    if(is_email($comment->comment_author_email)) {
-        $wp_email = 'no-reply@' . preg_replace('#^www\.#', '', strtolower($_SERVER['SERVER_NAME']));
+add_action('comment_unapproved_to_approved','kratos_comment_approved');
+function kratos_comment_approved($comment){
+    if(is_email($comment->comment_author_email)){
+        $wp_email = 'no-reply@'.preg_replace('#^www\.#','',strtolower($_SERVER['SERVER_NAME']));
         $to = trim($comment->comment_author_email);
         $post_link = get_permalink($comment->comment_post_ID);
         $subject = '[通知]您的留言已经通过审核';
         $message = '
+            <style>img.wp-smiley{width:auto!important;height:auto!important;max-height:8em!important;margin-top:-4px;display:inline}</style>
             <div style="background:#ececec;width: 100%;padding: 50px 0;text-align:center;">
             <div style="background:#fff;width:750px;text-align:left;position:relative;margin:0 auto;font-size:14px;line-height:1.5;">
                     <div style="zoom:1;padding:25px 40px;background:#518bcb; border-bottom:1px solid #467ec3;">
-                        <h1 style="color:#fff; font-size:25px;line-height:30px; margin:0;"><a href="' . get_option('home') . '" style="text-decoration: none;color: #FFF;">' . htmlspecialchars_decode(get_option('blogname'), ENT_QUOTES) . '</a></h1>
+                        <h1 style="color:#fff; font-size:25px;line-height:30px; margin:0;"><a href="'.get_option('home').'" style="text-decoration: none;color: #FFF;">'.htmlspecialchars_decode(get_option('blogname'),ENT_QUOTES).'</a></h1>
                     </div>
                 <div style="padding:35px 40px 30px;">
-                    <h2 style="font-size:18px;margin:5px 0;">Hi ' . trim($comment->comment_author) . ':</h2>
+                    <h2 style="font-size:18px;margin:5px 0;">Hi '.trim($comment->comment_author).':</h2>
                     <p style="color:#313131;line-height:20px;font-size:15px;margin:20px 0;">您有一条留言通过了管理员的审核并显示在文章页面，摘要信息请见下表。</p>
                         <table cellspacing="0" style="font-size:14px;text-align:center;border:1px solid #ccc;table-layout:fixed;width:500px;">
                             <thead>
@@ -46,42 +47,43 @@ function kratos_comment_approved($comment) {
                             </thead>
                             <tbody>
                                 <tr>
-                                    <td style="padding:5px 0;text-indent:8px;border:1px solid #eee;border-width:0 1px 1px 0;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;">《' . get_the_title($comment->comment_post_ID) . '》</td>
-                                    <td style="padding:5px 0;text-indent:8px;border:1px solid #eee;border-width:0 1px 1px 0;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;">'. trim($comment->comment_content) . '</td>
-                                    <td style="padding:5px 0;text-indent:8px;border:1px solid #eee;border-width:0 1px 1px 0;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;"><a href="'.get_comment_link( $comment->comment_ID ).'" style="color:#1E5494;text-decoration:none;vertical-align:middle;" target="_blank">查看留言</a></td>
+                                    <td style="padding:5px 0;text-indent:8px;border:1px solid #eee;border-width:0 1px 1px 0;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;">《'.get_the_title($comment->comment_post_ID).'》</td>
+                                    <td style="padding:5px 0;text-indent:8px;border:1px solid #eee;border-width:0 1px 1px 0;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;">'.convert_smilies(trim($comment->comment_content)).'</td>
+                                    <td style="padding:5px 0;text-indent:8px;border:1px solid #eee;border-width:0 1px 1px 0;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;"><a href="'.get_comment_link($comment->comment_ID).'" style="color:#1E5494;text-decoration:none;vertical-align:middle;" target="_blank">查看留言</a></td>
                                 </tr>
                             </tbody>
                         </table>
                         <br>
                     <div style="font-size:13px;color:#a0a0a0;padding-top:10px">该邮件由系统自动发出，如果不是您本人操作，请忽略此邮件。</div>
                     <div class="qmSysSign" style="padding-top:20px;font-size:12px;color:#a0a0a0;">
-                        <p style="color:#a0a0a0;line-height:18px;font-size:12px;margin:5px 0;">' . htmlspecialchars_decode(get_option('blogname'), ENT_QUOTES) . '</p>
-                        <p style="color:#a0a0a0;line-height:18px;font-size:12px;margin:5px 0;"><span style="border-bottom:1px dashed #ccc;" t="5" times="">' . date("Y年m月d日",time()) . '</span></p>
+                        <p style="color:#a0a0a0;line-height:18px;font-size:12px;margin:5px 0;">'.htmlspecialchars_decode(get_option('blogname'),ENT_QUOTES).'</p>
+                        <p style="color:#a0a0a0;line-height:18px;font-size:12px;margin:5px 0;"><span style="border-bottom:1px dashed #ccc;" t="5" times="">'.date("Y年m月d日",time()).'</span></p>
                     </div>
                 </div>
             </div>
         </div>';
-        $from = "From: \"" . htmlspecialchars_decode(get_option('blogname'), ENT_QUOTES) . "\" <$wp_email>";
-        $headers = "$from\nContent-Type: text/html; charset=" . get_option('blog_charset') . "\n";
-        wp_mail( $to, $subject, $message, $headers );
+        $from = "From: \"".htmlspecialchars_decode(get_option('blogname'),ENT_QUOTES)."\" <$wp_email>";
+        $headers = "$from\nContent-Type: text/html; charset=".get_option('blog_charset')."\n";
+        wp_mail($to,$subject,$message,$headers);
     }
 }
-function comment_mail_notify($comment_id) {
+function comment_mail_notify($comment_id){
     $comment = get_comment($comment_id);
-    $parent_id = $comment->comment_parent ? $comment->comment_parent : '';
+    $parent_id = $comment->comment_parent?$comment->comment_parent:'';
     $spam_confirmed = $comment->comment_approved;
-    if (($parent_id != '') && ($spam_confirmed != 'spam')) {
-        $wp_email = 'no-reply@' . preg_replace('#^www\.#', '', strtolower($_SERVER['SERVER_NAME']));
+    if(($parent_id!='')&&($spam_confirmed!='spam')){
+        $wp_email = 'no-reply@'.preg_replace('#^www\.#','',strtolower($_SERVER['SERVER_NAME']));
         $to = trim(get_comment($parent_id)->comment_author_email);
         $subject = '[通知]您的留言有了新的回复';
         $message = '
+            <style>img.wp-smiley{width:auto!important;height:auto!important;max-height:8em!important;margin-top:-4px;display:inline}</style>
             <div style="background:#ececec;width: 100%;padding: 50px 0;text-align:center;">
             <div style="background:#fff;width:750px;text-align:left;position:relative;margin:0 auto;font-size:14px;line-height:1.5;">
                     <div style="zoom:1;padding:25px 40px;background:#518bcb; border-bottom:1px solid #467ec3;">
-                        <h1 style="color:#fff; font-size:25px;line-height:30px; margin:0;"><a href="' . get_option('home') . '" style="text-decoration: none;color: #FFF;">' . htmlspecialchars_decode(get_option('blogname'), ENT_QUOTES) . '</a></h1>
+                        <h1 style="color:#fff; font-size:25px;line-height:30px; margin:0;"><a href="'.get_option('home').'" style="text-decoration: none;color: #FFF;">'.htmlspecialchars_decode(get_option('blogname'),ENT_QUOTES).'</a></h1>
                     </div>
                 <div style="padding:35px 40px 30px;">
-                    <h2 style="font-size:18px;margin:5px 0;">Hi ' . trim(get_comment($parent_id)->comment_author) . ':</h2>
+                    <h2 style="font-size:18px;margin:5px 0;">Hi '.trim(get_comment($parent_id)->comment_author).':</h2>
                     <p style="color:#313131;line-height:20px;font-size:15px;margin:20px 0;">您有一条留言有了新的回复，摘要信息请见下表。</p>
                         <table cellspacing="0" style="font-size:14px;text-align:center;border:1px solid #ccc;table-layout:fixed;width:500px;">
                             <thead>
@@ -94,25 +96,25 @@ function comment_mail_notify($comment_id) {
                             </thead>
                             <tbody>
                                 <tr>
-                                    <td style="padding:5px 0;text-indent:8px;border:1px solid #eee;border-width:0 1px 1px 0;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;">' . trim(get_comment($parent_id)->comment_content) . '</td>
-                                    <td style="padding:5px 0;text-indent:8px;border:1px solid #eee;border-width:0 1px 1px 0;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;">'. trim($comment->comment_content) . '</td>
-                                    <td style="padding:5px 0;text-indent:8px;border:1px solid #eee;border-width:0 1px 1px 0;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;">' . trim($comment->comment_author) . '</td>
-                                    <td style="padding:5px 0;text-indent:8px;border:1px solid #eee;border-width:0 1px 1px 0;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;"><a href="'.get_comment_link( $comment->comment_ID ).'" style="color:#1E5494;text-decoration:none;vertical-align:middle;" target="_blank">查看回复</a></td>
+                                    <td style="padding:5px 0;text-indent:8px;border:1px solid #eee;border-width:0 1px 1px 0;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;">'.convert_smilies(trim(get_comment($parent_id)->comment_content)).'</td>
+                                    <td style="padding:5px 0;text-indent:8px;border:1px solid #eee;border-width:0 1px 1px 0;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;">'.convert_smilies(trim($comment->comment_content)).'</td>
+                                    <td style="padding:5px 0;text-indent:8px;border:1px solid #eee;border-width:0 1px 1px 0;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;">'.trim($comment->comment_author).'</td>
+                                    <td style="padding:5px 0;text-indent:8px;border:1px solid #eee;border-width:0 1px 1px 0;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;"><a href="'.get_comment_link($comment->comment_ID).'" style="color:#1E5494;text-decoration:none;vertical-align:middle;" target="_blank">查看回复</a></td>
                                 </tr>
                             </tbody>
                         </table>
                         <br>
                     <div style="font-size:13px;color:#a0a0a0;padding-top:10px">该邮件由系统自动发出，如果不是您本人操作，请忽略此邮件。</div>
                     <div class="qmSysSign" style="padding-top:20px;font-size:12px;color:#a0a0a0;">
-                        <p style="color:#a0a0a0;line-height:18px;font-size:12px;margin:5px 0;">' . htmlspecialchars_decode(get_option('blogname'), ENT_QUOTES) . '</p>
-                        <p style="color:#a0a0a0;line-height:18px;font-size:12px;margin:5px 0;"><span style="border-bottom:1px dashed #ccc;" t="5" times="">' . date("Y年m月d日",time()) . '</span></p>
+                        <p style="color:#a0a0a0;line-height:18px;font-size:12px;margin:5px 0;">'.htmlspecialchars_decode(get_option('blogname'),ENT_QUOTES).'</p>
+                        <p style="color:#a0a0a0;line-height:18px;font-size:12px;margin:5px 0;"><span style="border-bottom:1px dashed #ccc;" t="5" times="">'.date("Y年m月d日",time()).'</span></p>
                     </div>
                 </div>
             </div>
         </div>';
-        $from = "From: \"" . htmlspecialchars_decode(get_option('blogname'), ENT_QUOTES) . "\" <$wp_email>";
-        $headers = "$from\nContent-Type: text/html; charset=" . get_option('blog_charset') . "\n";
-        wp_mail( $to, $subject, $message, $headers );
+        $from = "From: \"".htmlspecialchars_decode(get_option('blogname'),ENT_QUOTES)."\" <$wp_email>";
+        $headers = "$from\nContent-Type: text/html; charset=".get_option('blog_charset')."\n";
+        wp_mail($to,$subject,$message,$headers);
     }
 }
-add_action('comment_post', 'comment_mail_notify');
+add_action('comment_post','comment_mail_notify');
