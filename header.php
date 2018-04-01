2ed268730352573fd1fbbd51e0eef282088a2bab
小白-白 <xb@fczbl.vip>
2018-04-01 19:04:48 +0800

    Small changes

--- header.php
+++ header.php
@@ -30,7 +30,7 @@
     <style><?php
         echo '#offcanvas-menu{background:rgba('.kratos_option('mobi_color').')}';
         if(kratos_option('site_bw')) echo 'html{filter:grayscale(100%);-webkit-filter:grayscale(100%);-moz-filter:grayscale(100%);-ms-filter:grayscale(100%);filter:progid:DXImageTransform.Microsoft.BasicImage(grayscale=1);filter:gray;-webkit-filter:grayscale(1)}';
-        if(kratos_option('background_mode')=='image'&&!wp_is_mobile()&&!kratos_option('site_bw')) echo '@media(min-width:768px){.pagination>li>a{background-color:rgba(255,255,255,.8)}.kratos-hentry,.navigation div,.comments-area .comment-list li,#kratos-widget-area .widget,.comment-respond{background-color:rgba(255,255,255,.8)!important}.comment-list .children li{background-color:rgba(255,253,232,.7)!important}body.custom-background{background-image:url('.kratos_option('background_index_image').');background-size:cover;background-attachment:fixed}}';
+        if(kratos_option('background_mode')=='image'&&!wp_is_mobile()&&!kratos_option('site_bw')) echo '@media(min-width:768px){.pagination>li>a{background-color:rgba(255,255,255,.8)}.kratos-hentry,.navigation div,.comments-area .comment-list li,#kratos-widget-area .widget,.comment-respond{background-color:rgba(255,255,255,.75)!important}.comment-list .children li{background-color:rgba(255,253,232,.7)!important}body.custom-background{background-image:url('.kratos_option('background_index_image').');background-size:cover;background-attachment:fixed}}';
         if(kratos_option('head_mode')=='pic') echo '@media(max-width:768px){#kratos-header-section{background:rgba('.hex2rgb(kratos_option('banner_color')).','.kratos_option('banner_color_op').')}}@media(min-width:768px){.color-logo{display:none}.affix{top:54px}}'; ?>
     </style>
   </head>
